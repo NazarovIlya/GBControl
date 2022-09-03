021f86e194a4ff788e5e0679314d13885d1b414b
NazarovIlya <eliakohai@gmail.com>
2022-09-03 15:12:21 +0300

    Input and Check methods completed

--- FirstControl/Program.cs
+++ FirstControl/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строкформирует массив из строк,
+// длинна которых меньше либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// Использовать исключительно массивы.
+
+string[] InputArray()
+{
+    string[] arrayString = Console.ReadLine().Split(' ', ',');
+    return arrayString;
+}
+
+bool CheckInput(string[] array)
+{
+    if (array.Length - 1 == 0)
+    {
+        Console.WriteLine("Input error!");
+        return false;
+    }
+    return true;
+}
+
+
+
+
+
+// Console.Clear();
+string[] array = InputArray();
+
+
+
